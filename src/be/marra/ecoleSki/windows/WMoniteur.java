822457875b983943b5f39fda04f9394666e5642f
Kevin Marra <kevin.marra94@hotmail.fr>
2016-11-12 18:19:37 +0100

    Interface Moniteur

--- src/be/marra/ecoleSki/windows/WMoniteur.java
+++ src/be/marra/ecoleSki/windows/WMoniteur.java
@@ -1,6 +1,5 @@
 package be.marra.ecoleSki.windows;
 
-import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.Toolkit;
 
@@ -9,6 +8,8 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
 import be.marra.ecoleSki.Moniteur;
+import javax.swing.JButton;
+import com.toedter.calendar.JCalendar;
 
 public class WMoniteur extends JFrame {
 
@@ -22,19 +23,33 @@ public class WMoniteur extends JFrame {
 	//[start]Création de la fenêtre.
 	
 	public WMoniteur(Moniteur m) {
+		setResizable(false);
 		setTitle("Moniteur");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 531, 381);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JButton btnNewButton = new JButton("G\u00E9rer les accr\u00E9ditations");
+		btnNewButton.setBounds(10, 11, 243, 23);
+		contentPane.add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("Consulter les cours disponibles");
+		btnNewButton_1.setBounds(10, 45, 243, 23);
+		contentPane.add(btnNewButton_1);
+		
+		JButton btnNewButton_2 = new JButton("G\u00E9rer horaire");
+		btnNewButton_2.setBounds(264, 11, 243, 23);
+		contentPane.add(btnNewButton_2);
+		
+		JCalendar calendar = new JCalendar();
+		calendar.setBounds(10, 84, 497, 257);
+		contentPane.add(calendar);
 		
 		//Centre la fenêtre.
 		Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
 		setLocation(dim.width/2 - getWidth()/2, dim.height/2 - getHeight()/2);
 	}
-	
-	//[end]
-
 }
